7e9bfe273a551b806247360199b40b208a1fb5bd
Eddie Verdesoto <eddieverdesoto@hotmail.com>
2022-12-09 00:21:36 -0500

    Agregar archivos de proyecto.

---
+++ CursoDb/DataAccess/CoursesDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CursoDb.Models.DataModels;
+
+
+namespace CursoDb.DataAccess
+{
+    public class CoursesDBContext : DbContext
+    {
+        public CoursesDBContext(DbContextOptions<CoursesDBContext> options) : base(options)
+        {
+
+
+
+        }
+
+        // TODO: Add Dbsets (Tables of our Data base)
+        public DbSet<Course>? Courses { get; set; }
+    }
+}
---
+++ CursoDb/Models/DataModels/Course.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CursoDb.Models.DataModels
+{
+
+    public enum Level
+    {
+        Basic,
+        Intermediate,
+        Advanced
+    }
+    public class Course: BaseEntity
+    {
+        [Required, StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string fullDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string targetAudience { get; set; } = string.Empty;
+
+        [Required]
+        public string objectives { get; set; } = string.Empty;
+
+        [Required]
+        public string requirements { get; set; } = string.Empty;
+        
+        [Required]
+        public Level level { get; set; } = Level.Basic;
+
+    }
+}
